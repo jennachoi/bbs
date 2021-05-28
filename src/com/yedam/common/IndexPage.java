81d7877ddf1bd76ceb569a636a8cefd33e748b8f
admin <admin@YD02-08>
2021-05-28 17:56:15 +0900

    21-05-28

--- src/com/yedam/common/IndexPage.java
+++ src/com/yedam/common/IndexPage.java
@@ -4,8 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.yedam.product.serviceImpl.ProductServiceImpl;
-
 public class IndexPage implements DBCommand {
 
 	@Override
--- src/com/yedam/product/web/CartList.java
+++ src/com/yedam/product/web/CartList.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.yedam.common.DBCommand;
 import com.yedam.product.service.ProductService;
 import com.yedam.product.serviceImpl.ProductServiceImpl;
 import com.yedam.product.vo.CartVO;
-import com.yedam.product.vo.ProductVO;
 
 public class CartList implements DBCommand {
 
